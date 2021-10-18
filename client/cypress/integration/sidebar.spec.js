2f6864858dd9530a60312f4a0fbe6e97465a779a
JonasHendel <70843626+JonasHendel@users.noreply.github.com>
2021-10-18 17:31:31 +0200

    test

--- client/cypress/integration/sidebar.spec.js
+++ client/cypress/integration/sidebar.spec.js
@@ -3,8 +3,6 @@ describe('Navigation', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
 
-    cy.contains('Min. distance').click()
-
     cy.get('input').focus().type('vienna')
 
     cy.get('p').should('contain', 'VIE').then((p)=>{
